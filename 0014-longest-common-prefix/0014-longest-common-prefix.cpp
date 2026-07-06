377226fc2dcc03c167347a4e2b6fed94bd250885
Shoumik Das <dasshoumik95@gmail.com>
2026-07-07 00:04:06 +0530

    Time: 0 ms (100%), Space: 12 MB (39.91%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,17 +6,17 @@ public:
 
         sort(str.begin(), str.end());
 
-        string ans = "";
+        // string ans = "";
 
         int minLength = min(str[0].size(), str[str.size() - 1].size());
 
         for (int i = 0; i < minLength; i++) {
 
-            if (str[0][i] != str[str.size() - 1][i]) break;
+            if (str[0][i] != str[str.size() - 1][i]) return str[0].substr(0,i);
 
-            ans += str[0][i];
+            // ans += str[0][i];
         }
-
-        return ans;
+        return str[0].substr(0, minLength);
+        
     }
 };
