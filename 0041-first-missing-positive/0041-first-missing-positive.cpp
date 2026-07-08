6cef7d77025cd1a08bc4de837f2e0a27321df935
Shoumik Das <dasshoumik95@gmail.com>
2026-07-08 17:10:00 +0530

    Time: 58 ms (17.08%), Space: 55 MB (85.86%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> present(n+1, false);
+        int n = nums.size() ;
 
-        for (int num : nums) {
-            if (num > 0 && num <= n) {
-                present[num] = true;
-            }
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] < 0)
+                nums[i] = 0 ;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (!present[i]) {
-                return i;
+        for(int i = 0 ; i < n ; i++) {
+            int val = abs(nums[i]) ;
+            if(val >= 1 && val <= n) {
+                if(nums[val-1] > 0)
+                    nums[val-1] *= -1 ;
+                else if(nums[val-1] == 0)
+                    nums[val-1] = -(n+1) ;
             }
         }
-
-        return n+1 ;
+        for(int i = 0 ; i < n ; i++) {
+            if(nums[i] >= 0)
+                return i+1 ;
+        }
+        return n + 1 ;
     }
 };
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<bool> present(n+1, false);
+
+//         for (int num : nums) {
+//             if (num > 0 && num <= n) {
+//                 present[num] = true;
+//             }
+//         }
+
+//         for (int i = 1; i <= n; i++) {
+//             if (!present[i]) {
+//                 return i;
+//             }
+//         }
+
+//         return n+1 ;
+//     }
+// };
